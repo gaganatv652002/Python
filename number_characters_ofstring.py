fc5aefb0847a4360eda8188aaa1d556a433fdf43
GAGANA T V <147978720+gaganatv652002@users.noreply.github.com>
2023-12-13 10:22:31 +0530

    Create number_characters_ofstring.py

---
+++ number_characters_ofstring.py
@@ -0,0 +1,10 @@
+str=input("Enter a string:")
+count=0
+for i in range(0,len(str)):
+    if(str[1]!=""):
+        count=count+1
+print("Number of characters:",count)
+
+output:
+Enter a string:hello python
+Number of characters: 12
