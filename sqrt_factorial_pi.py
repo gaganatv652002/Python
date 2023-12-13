ceb576148b05e6f9b1f6f09d63d49f858e813b45
GAGANA T V <147978720+gaganatv652002@users.noreply.github.com>
2023-12-13 10:51:45 +0530

    sqrt_factorial_pi.py

---
+++ sqrt_factorial_pi.py
@@ -0,0 +1,11 @@
+import math
+number=int(input("Enter a number:"))
+print("square root of",number,":",math.sqrt(number),)
+print("factorial of",number,":",math.factorial(number))
+print("values of pi:",math.pi)
+
+output:
+Enter a number:4
+square root of 4 : 2.0
+factorial of 4 : 24
+values of pi: 3.141592653589793
